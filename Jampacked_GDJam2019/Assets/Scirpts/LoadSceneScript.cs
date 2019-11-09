e7b507f23f432188f6ced94876598ce63466fc19
Marcus Vine <marcus.vine@uoit.net>
2019-11-09 18:13:31 -0500

    fullscreen (not working)

---
+++ Jampacked_GDJam2019/Assets/Scirpts/LoadSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.fullScreen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
