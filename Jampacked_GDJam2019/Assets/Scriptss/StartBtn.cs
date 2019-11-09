64fa53db381fdacf96c457a50b10814e36dd6dd5
Leo Buono <leo.buono@ontariotechu.net>
2019-11-09 16:59:25 -0500

    Title With Title Card
    Title screen another version lmao

---
+++ Jampacked_GDJam2019/Assets/Scriptss/StartBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBtn : MonoBehaviour
+{
+    public void BeginGame()
+    {
+        print("epic");
+    }
+
+}
